93e7dbeb33cda43833e1735eeca2a2d168e73f4d
Fillan Lee <fillanlee@aol.com>
2019-02-09 03:14:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,16 +4,16 @@ def line (line)
   counter = 0 
   line_list = []
     
-  # if line.length == 0 
-  #   puts "The line is currently empty."
-  # else
-  #   while counter < line.length
-  #   line_list.push("#{counter+1}. #{line[counter]}")
-  #   counter += 1
-  #   end
-  # puts "The line is currently: #{line_list}"
-  # end
-  # line_list
+  if line.length == 0 
+    puts "The line is currently empty."
+  else
+    while counter < line.length
+    line_list.push("#{counter+1}. #{line[counter]}")
+    counter += 1
+    end
+   puts "The line is currently: #{line_list}"
+  end
+  line_list
 end
 
 
